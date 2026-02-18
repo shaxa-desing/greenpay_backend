6f6182b9148105d738882d6b84513b06b2a3d574
Shahruh <shahruhduschanov@gmail.com>
2026-02-18 22:44:18 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./greenpay.db"
+DATABASE_URL = os.getenv("postgresql://postgres:tIzRebfZKIkiiQwNziZAxuDWKGFKKjzn@postgres.railway.internal:5432/railway")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
