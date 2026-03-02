974b148a91300c62b81c03c658c1671dac01b78a
Shahruh <shahruhduschanov@gmail.com>
2026-03-02 20:56:43 +0500

    Remove location column from Tree model
    Removed 'location' column from Tree model.

--- models.py
+++ models.py
@@ -8,7 +8,6 @@ class Tree(Base):
     user_id = Column(Integer)
     user_name = Column(String)
     tree_type = Column(String)
-    location = Column(String)   # 🔥 SHU YO‘Q EDI
     latitude = Column(Float)
     longitude = Column(Float)
     status = Column(String, default="pending")
