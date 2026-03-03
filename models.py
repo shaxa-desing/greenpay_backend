60cb091ef79d1daea825a520b3cee77e444c13b2
Shahruh <shahruhduschanov@gmail.com>
2026-03-03 09:28:19 +0500

    Modify Tree model to include image_url and BigInteger user_id
    Updated user_id type to BigInteger and added image_url field.

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Float
-from database import Base
+from sqlalchemy import Column, Integer, String, Float, BigInteger
 
 class Tree(Base):
     __tablename__ = "trees"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
+    user_id = Column(BigInteger)
     user_name = Column(String)
     tree_type = Column(String)
-    location = Column(String)   # 🔴 MUHIM
+    location = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
+    image_url = Column(String)   # ⬅ YANGI
     status = Column(String, default="pending")
