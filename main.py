8343b973e478bbad242e8167fd3f1e1b7174690c
Shahruh <shahruhduschanov@gmail.com>
2026-03-03 09:31:53 +0500

    Implement file upload in create_tree endpoint
    Added file upload functionality for creating trees.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
+from fastapi import UploadFile, File, Form
+import shutil
+import os
 import models
 import schemas
 import crud
@@ -27,14 +30,46 @@ def get_db():
     finally:
         db.close()
 
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 # ===== TELEGRAM DATA =====
 @app.post("/trees/")
-def create_tree(tree: schemas.TreeCreate, db: Session = Depends(get_db)):
-    db_tree = models.Tree(**tree.dict())
+async def create_tree(
+    user_id: int = Form(...),
+    user_name: str = Form(...),
+    tree_type: str = Form(...),
+    location: str = Form(...),
+    latitude: float = Form(...),
+    longitude: float = Form(...),
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+
+    file_path = f"{UPLOAD_DIR}/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    image_url = file_path
+
+    db_tree = models.Tree(
+        user_id=user_id,
+        user_name=user_name,
+        tree_type=tree_type,
+        location=location,
+        latitude=latitude,
+        longitude=longitude,
+        image_url=image_url,
+        status="pending"
+    )
+
     db.add(db_tree)
     db.commit()
     db.refresh(db_tree)
-    return db_tree
+
+    return {"message": "Saved"}
 
 # ===== BARCHA DARAxtLAR =====
 @app.get("/trees/")
@@ -85,6 +120,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def read_index():
     return FileResponse("static/index.html")
+
 
 
 
