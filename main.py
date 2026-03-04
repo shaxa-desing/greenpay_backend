d9d48409bf8585c33d5446dc968f8f807dfaaa53
Shahruh <shahruhduschanov@gmail.com>
2026-03-04 12:46:00 +0500

    Refactor tree creation to use Pydantic model

--- main.py
+++ main.py
@@ -1,41 +1,26 @@
-from fastapi import FastAPI, Form
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 trees = []
 
-@app.get("/")
-def root():
-    return {"status": "working"}
+class Tree(BaseModel):
+    user_id: int
+    user_name: str
+    phone: str
+    tree_type: str
+    latitude: float
+    longitude: float
+    photo: str
+
+
+@app.post("/trees/")
+def create_tree(tree: Tree):
+    trees.append(tree)
+    return {"status": "saved"}
+
 
 @app.get("/trees/")
 def get_trees():
     return trees
-
-@app.post("/trees/")
-def create_tree(
-    user_id: int = Form(...),
-    user_name: str = Form(...),
-    tree_type: str = Form(...),
-    latitude: float = Form(...),
-    longitude: float = Form(...)
-):
-    tree = {
-        "user_id": user_id,
-        "user_name": user_name,
-        "tree_type": tree_type,
-        "latitude": latitude,
-        "longitude": longitude
-    }
-
-    trees.append(tree)
-    return {"message": "saved"}
