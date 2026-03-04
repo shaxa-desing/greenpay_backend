b3892dc50e88f17b64049cf924989469d3469997
Shahruh <shahruhduschanov@gmail.com>
2026-03-04 18:04:44 +0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -2,21 +2,17 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 
+def create_tree(db: Session, tree: schemas.Tree):
 
-def create_tree(db: Session, tree: schemas.TreeCreate):
-    db_tree = models.Tree(
-        user_id=tree.user_id,
-        user_name=tree.user_name,
-        tree_type=tree.tree_type,
-        latitude=tree.latitude,
-        longitude=tree.longitude,
-        status="pending"
-    )
+    db_tree = models.Tree(**tree.dict())
+
     db.add(db_tree)
     db.commit()
     db.refresh(db_tree)
+
     return db_tree
 
 
 def get_trees(db: Session):
+
     return db.query(models.Tree).all()
