a719253e0b96f3608ed74e23a99b29fe582b58ed
Shahruh <shahruhduschanov@gmail.com>
2026-03-04 18:04:00 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Float, BigInteger
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
 
 class Tree(Base):
     __tablename__ = "trees"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(BigInteger)
+    user_id = Column(Integer)
     user_name = Column(String)
+    phone = Column(String)
     tree_type = Column(String)
-    location = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
-    image_url = Column(String)   # ⬅ YANGI
-    status = Column(String, default="pending")
+    photo = Column(String)
