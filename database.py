f8a250b16ae2e692776f9b267c0f698a3d055289
Shahruh <shahruhduschanov@gmail.com>
2026-03-04 18:03:43 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
