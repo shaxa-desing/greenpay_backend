2586550dfc058f75136a003038ad7419d4b4c3f5
Shahruh <shahruhduschanov@gmail.com>
2026-03-04 18:43:19 +0500

    Update models.py

--- models.py
+++ models.py
@@ -2,13 +2,19 @@ from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class Tree(Base):
+
     __tablename__ = "trees"
 
     id = Column(Integer, primary_key=True, index=True)
+
     user_id = Column(Integer)
     user_name = Column(String)
+
     phone = Column(String)
+
     tree_type = Column(String)
+
     latitude = Column(Float)
     longitude = Column(Float)
+
     photo = Column(String)
