55aa22f5cf389efc88baf9a350b3b33bb647d33b
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 17:39:41 +0500

    Update models.py

--- models.py
+++ models.py
@@ -2,19 +2,21 @@ from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class Tree(Base):
-
     __tablename__ = "trees"
-
     id = Column(Integer, primary_key=True, index=True)
-
     user_id = Column(Integer)
     user_name = Column(String)
-
     phone = Column(String)
-
     tree_type = Column(String)
-
     latitude = Column(Float)
     longitude = Column(Float)
-
     photo = Column(String)
+    status = Column(String, default="Kutilmoqda") # Status qo'shildi
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, unique=True)
+    user_name = Column(String)
+    card = Column(String, nullable=True)
+    phone_pay = Column(String, nullable=True)
