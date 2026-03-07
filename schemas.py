3c28fca318899fece455a257df5b7df1cb9a4a40
Shahruh <shahruhduschanov@gmail.com>
2026-03-07 22:07:39 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -5,23 +5,13 @@ class UserCreate(BaseModel):
     user_id: int
     user_name: str
     username: Optional[str] = None
-    phone: str
+    phone: Optional[str] = None
 
-class Tree(BaseModel):
+class TreeCreate(BaseModel):
     user_id: int
-    user_name: str # Dashboardda null chiqmasligi uchun shart
-    phone: str
+    user_name: str
+    phone: Optional[str] = None
     tree_type: str
     latitude: float
     longitude: float
     photo: str
-    
-class PaymentUpdate(BaseModel):
-    user_id: int
-    card: Optional[str] = None
-    phone_pay: Optional[str] = None
-
-    class Config:
-        from_attributes = True
-
-
