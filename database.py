0c63c68f32ba01598f27b7d7d2cae191e930442c
Shahruh <shahruhduschanov@gmail.com>
2026-03-15 15:16:52 +0500

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,6 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Ushbu funksiya main.py uchun juda muhim
 def get_db():
     db = SessionLocal()
     try:
